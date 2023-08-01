f761da429288c959f835db940c254dbd151b3f20
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2023-08-01 13:06:40 -0300

    Pequenas correções no script de configuração

--- config/2_config.sh
+++ config/2_config.sh
@@ -3,6 +3,9 @@
 file="/opt/sql/__create__.txt";
 
 while read t; do
+  if [[ -z "${t%?}" || ${t:0:1} == '#' ]]; then
+    continue;
+  fi
   table="/opt/sql/${t%?}.sql";
   echo -e "Trying to create the table $table\n";
   psql -U postgres -d andifes -f $table;
