d836115513c650987387b455b4a9e36163133824
Jonathan Braian Dias Vaz <jonathanbd.vaz@gmail.com>
2023-08-02 17:10:27 -0300

    Atualizando tabela do DOCENTE ORIENTADOR

--- sql/docente_orientador.sql
+++ sql/docente_orientador.sql
@@ -3,7 +3,7 @@
 
 CREATE TABLE docente_orientador
 (
-    cpf CHAR(11) PRIMARY KEY,
+cpf CHAR(11) PRIMARY KEY,
 
     disp_orientandos_grad INTEGER NOT NULL,
     disp_orientandos_ce INTEGER NOT NULL,
@@ -17,24 +17,23 @@ CREATE TABLE docente_orientador
         FOREIGN KEY (cpf)
             REFERENCES especialista (cpf)
                 ON DELETE CASCADE
-                ON UPDATE CASCADE
+                ON UPDATE CASCADE,
     
-    -- CONSTRAINT fk_adesao_docente_oc
-    --     FOREIGN KEY (adesao_docente_oc)
-    --         REFERENCES ficha_base (id)
-    --             ON DELETE CASCADE
-    --             ON UPDATE CASCADE
---
-    --CONSTRAINT fk_gestor_oferta
-    --    FOREIGN KEY (gestor_oferta)
-    --        REFERENCES especialista (cpf)
-    --            ON DELETE CASCADE
-    --            ON UPDATE CASCADE,
+    CONSTRAINT fk_adesao_docente_oc
+        FOREIGN KEY (adesao_docente_oc)
+            REFERENCES ficha_base (id)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE,
 
-    --CONSTRAINT fk_coord_administrativo
-    --    FOREIGN KEY (coord_administrativo)
-    --         REFERENCES especialista (cpf)
-    --            ON DELETE CASCADE
-    --            ON UPDATE CASCADE,
-    
+    CONSTRAINT fk_gestor_oferta
+        FOREIGN KEY (gestor_oferta)
+            REFERENCES gestor_oferta (cpf)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE,
+
+    CONSTRAINT fk_coord_administrativo
+        FOREIGN KEY (coord_administrativo)
+             REFERENCES coord_administrativo (cpf)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE
 );
