e839b58669c0e74464cff4434a9101a54335c5ef
Rafael Naoki Arakaki Uyeta <rafa.naoki@gmail.com>
2023-08-02 03:48:23 -0300

    Adicionando a tabela dia_aula_turma

---
+++ sql/dia_aula_turma.sql
@@ -0,0 +1,15 @@
+CREATE TABLE dia_aula_turma (
+    dia_semana INTEGER NOT NULL, --flag para os dias da semana?
+    horario_inicio TIME NOT NULL,
+    horario_termino TIME NOT NULL, --Até aqui é tudo atribtuo, que precisam fazer parte da chave por ser uma tabela de um atributo multivalorado
+    id_turma INTEGER NOT NULL,
+
+    CONSTRAINT fk_turma
+        FOREIGN KEY id_turma
+        REFERENCES turma_ofertada_idioma (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+
+    CONSTRAINT pk_dia_aula_turma 
+        PRIMARY KEY (dia_semana, horario_inicio, horario_termino, id_turma)
+)
