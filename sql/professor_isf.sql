6f625603a0835e49ed1313edb8ffe19a8499467a
Lara S. Rodrigues <110498717+lsr94@users.noreply.github.com>
2023-08-02 09:26:31 -0300

    Update professor_isf.sql
    Credenciamento de Professores ISF

--- sql/professor_isf.sql
+++ sql/professor_isf.sql
@@ -1,10 +1,20 @@
--- FIXME Completar isso...
+-- Tabela referente as informações do Professor ISF
+-- Ao atributo categoria pode ser atribuído dois valores distintos: cursista ou aluno da graduação
 
 CREATE TABLE professor_isf
 (
     cpf CHAR(11) PRIMARY KEY,
+    titulo_formulario VARCHAR(100) NOT NULL,
+    data_inicio_formulario DATE NOT NULL,
+    categoria VARCHAR(25) NOT NULL,
 
-    CONSTRAINT fk_pessoa
-        FOREIGN KEY (cpf)
-            REFERENCES pessoa(cpf)
-)
+    CONSTRAINT fk_pessoa 
+        FOREIGN KEY (cpf) 
+            REFERENCES pessoa(cpf) 
+            ON DELETE CASCADE 
+            ON UPDATE CASCADE,
+    
+    CONSTRAINT fk_formulario 
+        FOREIGN KEY (titulo_formulario, data_inicio_formulario) 
+            REFERENCES formulario
+);
