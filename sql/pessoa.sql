9745f0f2d34014ea9a8a4a9280b8e2ca64cb9d2e
LucasMBalieiro <lucas.macbali@gmail.com>
2023-08-02 14:59:19 -0300

    fix: pessoa, email

--- sql/pessoa.sql
+++ sql/pessoa.sql
@@ -14,6 +14,6 @@ CREATE TABLE pessoa
     CONSTRAINT fk_cep
         FOREIGN KEY (cep)
             REFERENCES endereco(cep)
-            ON DELETE NO ACTION
+            ON DELETE RESTRICT 
             ON UPDATE NO ACTION 
 );
