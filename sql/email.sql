a682917f79acbc8bf5b69047587556f2bde3fc23
Rafael Naoki Arakaki Uyeta <rafa.naoki@gmail.com>
2023-08-02 01:56:51 -0300

    Att

--- sql/email.sql
+++ sql/email.sql
@@ -1,4 +1,11 @@
 CREATE TABLE email
 (
-    endereco VARCHAR(255) PRIMARY KEY
-);
+    endereco VARCHAR(255) PRIMARY KEY,
+    CPF_pessoa VARCHAR(11) NOT NULL,
+
+    CONSTRAINT fk_CPF   
+        FOREIGN KEY (endereco)
+        REFERENCES pessoa (cpf)
+        ON DELETE CASCADE
+        ON DELETE UPDATE
+)
