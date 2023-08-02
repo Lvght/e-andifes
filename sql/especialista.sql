dfff51eaded5e65e4d740a59050c7ab1acdf3b82
Bowie <marketing@letscode.com.br>
2023-08-02 10:50:05 -0300

    Removendo o campo 'credenciado_em' do especialista, uma vez que fica na entidade 'ficha_credenciamento_especialista

--- sql/especialista.sql
+++ sql/especialista.sql
@@ -10,7 +10,6 @@ CREATE TABLE IF NOT EXISTS especialista
     setor_atuacao VARCHAR(255) NOT NULL,
     lattes VARCHAR(255) UNIQUE NOT NULL,
     maior_titulacao VARCHAR(100) NOT NULL,
-    credenciado_em VARCHAR(60),
     CONSTRAINT fk_especialista_pessoa FOREIGN KEY (cpf) REFERENCES pessoa(cpf),
     CONSTRAINT pk_especialista PRIMARY KEY (cpf)
 );
