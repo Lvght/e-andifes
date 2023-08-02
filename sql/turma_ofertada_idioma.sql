3803fe300bae3839284f1805e306f6cb850fdbf5
thales <thaleswcm@estudante.ufscar.br>
2023-08-02 14:37:31 -0300

    Adiconando docente_especialista

--- sql/turma_ofertada_idioma.sql
+++ sql/turma_ofertada_idioma.sql
@@ -2,21 +2,7 @@ CREATE TABLE turma_ofertada_idioma
 (
     id SERIAL PRIMARY KEY
 
-    DataIngresso DATE,
-    CódigoCurso INT,
-    NomeCurso VARCHAR(100),
-    Idioma VARCHAR(50),
-    DiaHorarioAulas VARCHAR(100),
-    DiaDaSemana VARCHAR(20),
-    HorarioInicio TIME,
-    TipoOferta VARCHAR(50),
-    DataFim DATE,
-    HorarioTermino TIME,
-    NroInscritos INT,
-    DataInicio DATE,
-    Titulo VARCHAR(200),
-    CPF_ProfessorISF VARCHAR(14),
-    PRIMARY KEY (DataIngresso, CódigoCurso)
+   
 );
 
 
