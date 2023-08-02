b5b2fad89647456629a2ebcea027b10903011035
Miguel Chinellato <86507835+MiguelChinellato@users.noreply.github.com>
2023-08-02 14:48:27 -0300

    feat: adicionando surrogate para chave primária composta de endereco.sql

--- sql/endereco.sql
+++ sql/endereco.sql
@@ -1,7 +1,9 @@
 CREATE TABLE endereco (
+    id SERIAL PRIMARY KEY,
     codigo_postal VARCHAR(20) NOT NULL,
     cidade VARCHAR(100) NOT NULL,
     estado VARCHAR(50),
     pais VARCHAR(50) NOT NULL,
-    PRIMARY KEY (codigo_postal, pais)
+    
+    CONSTRAINT unique_endereco UNIQUE (codigo_postal, pais)
 );
