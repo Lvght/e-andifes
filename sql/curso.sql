5bcd9716bd23cb9f30029ac5662e59d68bbd04bb
thales <thaleswcm@estudante.ufscar.br>
2023-08-02 15:42:22 -0300

    Completando turma_ofertada_idiomas

--- sql/curso.sql
+++ sql/curso.sql
@@ -1,9 +1,11 @@
 CREATE TABLE IF NOT EXISTS curso
 (
+    id SERIAL PRIMARY KEY,
+    
     nome VARCHAR(100) NOT NULL,
     codigo INTEGER NOT NULL,
     idioma VARCHAR(50) NOT NULL,
     carga_horaria INTEGER,
     link_plano_de_ensino TEXT,
-    CONSTRAINT pk_curso PRIMARY KEY (nome, codigo)
+
 );
