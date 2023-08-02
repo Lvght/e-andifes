d5f9c29c6230adb5f5039b0d870dcc6fdf0b69a0
PedroGSantos <pedrogsantos_10@hotmail.com>
2023-08-02 01:45:34 -0300

    feat: adding nucleo table

---
+++ sql/endereco_nucleo.sql
@@ -0,0 +1,16 @@
+CREATE TABLE endereco_nucleo
+(
+    id          SERIAL PRIMARY KEY,
+    FK_IES      VARCHAR(50) NOT NULL,
+    logradouro  VARCHAR(100) NOT NULL,
+    FK_CEP      VARCHAR(8) NOT NULL,
+
+    CONSTRAINT fk_ies_endereco_nucleo
+        FOREIGN KEY (FK_IES)
+            REFERENCES instituicao_ensino_superior (PK_IES)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
+
+    CONSTRAINT unique_ies_endereco_nucleo
+        UNIQUE (FK_IES)
+);
