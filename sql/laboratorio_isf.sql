5ac080046929d084395a3082ec9dc40a0e1eaf65
Jorge Pires <jlmp2002@gmail.com>
2023-08-02 11:46:40 -0300

    Adding laboratorio ISF

---
+++ sql/laboratorio_isf.sql
@@ -0,0 +1,22 @@
+CREATE TABLE laboratorio_isf
+(
+    id            SERIAL PRIMARY KEY,
+    FK_IES        VARCHAR(50) NOT NULL,
+    logradouro    VARCHAR(100) NOT NULL,
+    FK_CEP        VARCHAR(8) NOT NULL,
+
+    CONSTRAINT fk_cep
+        FOREIGN KEY (FK_CEP)
+            REFERENCES endereco (CEP)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
+
+    CONSTRAINT fk_instituicao_ensino_superior
+        FOREIGN KEY (FK_IES)
+            REFERENCES instituicao_ensino_superior (PK_IES)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
+
+    CONSTRAINT unique_endereco_instituicao
+        UNIQUE (FK_CEP, FK_IES)
+);
