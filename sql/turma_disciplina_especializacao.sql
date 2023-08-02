b4fa369f33f8f9405d9dc50983caee3ea8a3779e
CarlosFontaneli <carlosfontaneli42@gmail.com>
2023-08-02 15:27:56 -0300

    Adicionando tabelas gestor_oferta e turma_disciplina_especializacao,user 3

---
+++ sql/turma_disciplina_especializacao.sql
@@ -0,0 +1,13 @@
+CREATE TABLE turma_disciplina_especializacao (
+  id_disc_especilizacao SERIAL,
+  codigo_turma SERIAL,
+  data_inicio  DATE NOT NULL,
+  data_fim DATE NULL, 
+
+  CONSTRAINT disciplina_especializacao
+        FOREIGN KEY (id_disc_especilizacao)
+            REFERENCES disciplina_especializacao (id)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE,
+  CONSTRAINT ck_turma_disciplina_especializacao PRIMARY KEY (id_disc_especilizacao,codigo_turma)
+)
