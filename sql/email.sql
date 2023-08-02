14874431d734374210ac7ec87f283330809d7a9a
LucasMBalieiro <lucas.macbali@gmail.com>
2023-08-02 14:59:19 -0300

    fix: pessoa, email

--- sql/email.sql
+++ sql/email.sql
@@ -1,4 +1,26 @@
+-- Tabela que relaciona as entidades de Pessoa e E-Mail.
+-- Uma mesma pessoa pode ter mais de um e-mail.
+
 CREATE TABLE email
 (
-    endereco VARCHAR(255) PRIMARY KEY
+    id        SERIAL PRIMARY KEY,
+    pessoa    CHAR(11)     NOT NULL,
+    email     VARCHAR(255) NOT NULL,
+
+    -- Indica qual o e-mail preferido para receber notificações.
+    principal BOOLEAN      NOT NULL DEFAULT TRUE,
+
+    CONSTRAINT unique_email UNIQUE (email),
+
+    CONSTRAINT fk_email
+        FOREIGN KEY (email)
+            REFERENCES email (endereco)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
+
+    CONSTRAINT fk_pessoa
+        FOREIGN KEY (pessoa)
+            REFERENCES pessoa (cpf)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
 );
