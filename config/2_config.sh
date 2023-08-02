61f2e2e0045daa8a3289aa0136fe46e9f2aa6bd0
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2023-08-02 16:35:09 -0300

    Agora erros são fatais

--- config/2_config.sh
+++ config/2_config.sh
@@ -7,5 +7,11 @@ while read t; do
   fi
   table="/opt/sql/$t.sql";
   echo -e "Trying to create the table $table\n";
-  psql -U postgres -d andifes -f $table;
+  ret=$(psql -U postgres -d andifes -v ON_ERROR_STOP=on -f $table);
+
+  if [[ -z $ret ]]; then
+    echo -e "Error creating the table $table\n";
+    echo -e "Ret code is $ret\n";
+    exit 1;
+  fi
 done < "$file";
