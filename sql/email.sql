062924a8e187185acc4b9c5d5ae0f953829bf741
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2023-08-02 00:07:56 -0300

    Revertidas alterações conflitantes

--- sql/email.sql
+++ sql/email.sql
@@ -1,11 +1,4 @@
 CREATE TABLE email
 (
-    endereco VARCHAR(255) PRIMARY KEY,
-    CPF_pessoa VARCHAR(11) NOT NULL,
-
-    CONSTRAINT fk_CPF   
-        FOREIGN KEY (endereco)
-        REFERENCES pessoa (cpf)
-        ON DELETE CASCADE
-        ON DELETE UPDATE
-)
+    endereco VARCHAR(255) PRIMARY KEY
+);
--- sql/telefone.sql
+++ sql/telefone.sql
@@ -1,16 +1,9 @@
-CREATE TABLE telefone 
+CREATE TABLE telefone
 (
     id SERIAL PRIMARY KEY,
-    codigo_area VARCHAR(4) NOT NULL,
-    codigo_pais VARCHAR(4) NOT NULL,
-    numero VARCHAR(9) NOT NULL,
-    CPF_pessoa VARCHAR(11) NOT NULL,
+    codigo_pais SMALLINT NOT NULL,
+    codigo_area SMALLINT NOT NULL,
+    numero INTEGER NOT NULL,
 
     CONSTRAINT unique_by_locale UNIQUE (codigo_pais, codigo_area, numero)
-
-    CONSTRAINT fk_CPF
-        FOREIGN KEY (CPF_pessoa)
-        REFERENCES CPF (pessoa)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE;
-)
+);
