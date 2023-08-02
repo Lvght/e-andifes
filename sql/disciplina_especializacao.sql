dc2ff123be112a0a6dcef9c3629733583b8d9b54
Pedro Lealdini <58371590+Kommnenos@users.noreply.github.com>
2023-08-02 17:57:20 +0000

    arrumando errinhos

--- sql/disciplina_especializacao.sql
+++ sql/disciplina_especializacao.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS disciplinaespecializacao
+CREATE TABLE IF NOT EXISTS disciplina_especializacao
 (
-    id CHAR(100),
+    id SERIAL(100),
     nome VARCHAR(100) NOT NULL,
     carga_horaria VARCHAR(255) NOT NULL,
     eixo_tematico VARCHAR(255) NOT NULL,
     componente_curricular VARCHAR(255) NOT NULL,
-    link_projeto_pedagogico VARCHAR(10) NOT NULL,
+    link_projeto_pedagogico VARCHAR(255) NOT NULL,
     tipo_disciplina VARCHAR(100) NOT NULL,
     nome VARCHAR(255) UNIQUE NOT NULL,
    
