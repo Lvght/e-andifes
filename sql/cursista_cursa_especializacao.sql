59e7ce819eec96144067821b8da39c98ac623eac
Rafael Naoki Arakaki Uyeta <rafa.naoki@gmail.com>
2023-08-03 16:04:18 -0300

    corrigindo o cursista cursa especialização

--- sql/cursista_cursa_especializacao.sql
+++ sql/cursista_cursa_especializacao.sql
@@ -1,19 +1,21 @@
 CREATE TABLE cursista_cursa_especializacao(
+    id SERIAL principal KEY,
+    codigo_turma_especializacao VARCHAR(16),
+    cpf_cursista CHAR(11),
+    horas_praticas_validadas INTEGER,
+    horas_praticas_solicitadas INTEGER,
 
-codigo_turma_especializacao VARCHAR(16) PRIMARY KEY,
-cpf_cursista CHAR(11) PRIMARY KEY,
-horas_praticas_validadas INTEGER,
-horas_praticas_solicitadas INTEGER,
+    CONSTRAINT unique_codigo_cpf UNIQUE (codigo_turma_especializacao,cpf_cursista),
 
-CONSTRAINT fk_codigo_turma_especializacao
-    FOREIGN KEY codigo_turma_especializacao
-        REFERENCES turma_disciplina_especializacao(codigo)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
+    CONSTRAINT fk_codigo_turma_especializacao
+        FOREIGN KEY codigo_turma_especializacao
+            REFERENCES turma_disciplina_especializacao(codigo)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
 
-CONSTRAINT fk_CPF
-    FOREIGN KEY cpf_cursista
-        REFERENCES pessoa(cpf)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+    CONSTRAINT fk_CPF
+        FOREIGN KEY cpf_cursista
+            REFERENCES pessoa(cpf)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
 );
