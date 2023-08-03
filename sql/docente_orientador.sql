9b17e2666769c35914b1731b2e462c457a99fe8a
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2023-08-02 22:25:46 -0300

    Removida relação desnecessária
    Co-authored-by: Alisson Nunes <alynva@gmail.com>
    Co-authored-by: giovani <gi@guidini.com.br>

--- sql/docente_orientador.sql
+++ sql/docente_orientador.sql
@@ -9,19 +9,12 @@ cpf CHAR(11) PRIMARY KEY,
     disp_orientandos_ce INTEGER NOT NULL,
     pos_orientar_outras_ies CHAR(3) NOT NULL,
 
-    adesao_docente_oc INTEGER NOT NULL,
     gestor_oferta CHAR(11) NOT NULL,
     coord_administrativo CHAR(11) NOT NULL,
 
     CONSTRAINT fk_especialista
         FOREIGN KEY (cpf)
             REFERENCES especialista (cpf)
-                ON DELETE CASCADE
-                ON UPDATE CASCADE,
-    
-    CONSTRAINT fk_adesao_docente_oc
-        FOREIGN KEY (adesao_docente_oc)
-            REFERENCES ficha_base (id)
                 ON DELETE CASCADE
                 ON UPDATE CASCADE,
 
