f21f0647a2442e66754ff52171e790543896c1b5
Rafael Naoki Arakaki Uyeta <rafa.naoki@gmail.com>
2023-08-03 15:59:20 -0300

    arrumando os email

--- sql/email.sql
+++ sql/email.sql
@@ -1,26 +1,4 @@
--- Tabela que relaciona as entidades de Pessoa e E-Mail.
--- Uma mesma pessoa pode ter mais de um e-mail.
-
 CREATE TABLE email
 (
-    id        SERIAL PRIMARY KEY,
-    pessoa    CHAR(11)     NOT NULL,
-    email     VARCHAR(255) NOT NULL,
-
-    -- Indica qual o e-mail preferido para receber notificações.
-    principal BOOLEAN      NOT NULL DEFAULT TRUE,
-
-    CONSTRAINT unique_email UNIQUE (email),
-
-    CONSTRAINT fk_email
-        FOREIGN KEY (email)
-            REFERENCES email (endereco)
-            ON DELETE CASCADE
-            ON UPDATE CASCADE,
-
-    CONSTRAINT fk_pessoa
-        FOREIGN KEY (pessoa)
-            REFERENCES pessoa (cpf)
-            ON DELETE CASCADE
-            ON UPDATE CASCADE
+    endereco VARCHAR(255) PRIMARY KEY
 );
--- sql/pessoa_email.sql
+++ sql/pessoa_email.sql
@@ -3,11 +3,20 @@
 
 CREATE TABLE pessoa_email
 (
-    email     VARCHAR(255) NOT NULL PRIMARY KEY,
+    id        SERIAL PRIMARY KEY,
     pessoa    CHAR(11)     NOT NULL,
+    email     VARCHAR(255) NOT NULL,
 
     -- Indica qual o e-mail preferido para receber notificações.
     principal BOOLEAN      NOT NULL DEFAULT TRUE,
+
+    CONSTRAINT unique_email UNIQUE (email),
+
+    CONSTRAINT fk_email
+        FOREIGN KEY (email)
+            REFERENCES email (endereco)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
 
     CONSTRAINT fk_pessoa
         FOREIGN KEY (pessoa)
