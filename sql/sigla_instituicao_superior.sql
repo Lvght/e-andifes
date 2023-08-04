2133f84e25565135c10db428b1a2c0f795c95c25
PedroGSantos <pedrogsantos_10@hotmail.com>
2023-08-04 19:31:53 -0300

    fix: adding surrogate on sigla to differents universities

--- sql/sigla_instituicao_superior.sql
+++ sql/sigla_instituicao_superior.sql
@@ -1,3 +1,11 @@
 CREATE TABLE sigla_instituicao_superior (
-    sigla VARCHAR(10) PRIMARY KEY NOT NULL UNIQUE
+    id SERIAL PRIMARY KEY,
+    sigla VARCHAR(10) NOT NULL
 );
+
+COMMENT ON TABLE sigla_instituicao_superior
+    IS 'Uma ies pode ser cadastrada mais de uma vez,'
+    'dado que ela pode ter mais de um campus. Como esses campi'
+    'tem a mesma sigla, criamos essa tabela separada. Alem disso,'
+    'foi adicionado um surrogate, pois universidades diferentes podem ter'
+    'a mesma sigla';
