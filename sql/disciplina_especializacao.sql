341a6f59232163e1313db865617fa66b53e1add9
Pedro Lealdini <58371590+Kommnenos@users.noreply.github.com>
2023-08-08 11:37:17 -0300

    Update disciplina_especializacao.sql

--- sql/disciplina_especializacao.sql
+++ sql/disciplina_especializacao.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS disciplina_especializacao
 (
     id SERIAL,
-    carga_horaria VARCHAR(255) NOT NULL,
+    carga_horaria INTEGER NOT NULL,
     eixo_tematico VARCHAR(255) NOT NULL,
     componente_curricular VARCHAR(255) NOT NULL,
     link_projeto_pedagogico VARCHAR(255) NOT NULL,
