b4bf9143023c1b92fc621d4e9bbe03298121d27c
Jorge Pires <jlmp2002@gmail.com>
2023-08-10 01:14:20 -0300

    fix: telefone
    Adiciona o atributo institucional que será FALSE por padrão, indicando que o telefone não é institucional.

--- database/sql/telefone.sql
+++ database/sql/telefone.sql
@@ -4,6 +4,7 @@ CREATE TABLE telefone
     codigo_pais SMALLINT NOT NULL,
     codigo_area SMALLINT NOT NULL,
     numero INTEGER NOT NULL,
+    institucional BOOLEAN DEFAULT FALSE,
 
     CONSTRAINT unique_by_locale UNIQUE (codigo_pais, codigo_area, numero)
 );
