d8a16af8050fdb927b0657ffdf2e6304f4ca54fb
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2023-08-24 18:52:48 -0300

    Seeding automático

---
+++ interface/seed/endereco.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    [
+        'id' => 1,
+        'codigo_postal' => '11111111',
+        'cidade' => 'São Carlos',
+        'estado' => 'SP',
+        'pais' => 'Brasil'
+    ],
+    [
+        'id' => 2,
+        'codigo_postal' => '11111112',
+        'cidade' => 'São Carlos',
+        'estado' => 'SP',
+        'pais' => 'Brasil'
+    ]
+];
--- interface/src/public/dashboard.php
+++ interface/src/public/dashboard.php
@@ -2,5 +2,19 @@
 
 include $_ENV['HOME'] . '/inc/inject.php';
 
+$query = pg_query(getConnection(), "SELECT * FROM pessoa");
+$result = pg_fetch_all($query);
+
+// checks if result is not empty.
+if ($result) {
+    $keys = array_keys($result[0]);
+} else {
+    $keys = [];
+}
+
 $template = $twig->load('dashboard.twig');
-echo $template->render(['username' => $_SESSION['username']]);
+echo $template->render([
+    'username' => $_SESSION['username'],
+    'pessoas' => $result,
+    'keys' => $keys
+]);
