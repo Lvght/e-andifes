8d4c8615f33d85f222d5279af2dd2f9f06e88971
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2023-08-26 09:46:01 -0300

    Trigger Instead Of para inserir dados na view
    Co-authored-by: NataliaBachiega <nataliabachiega@hotmail.com>

---
+++ database/sql/triggers/after_insert_view_pessoa.sql
@@ -0,0 +1,53 @@
+CREATE OR REPLACE FUNCTION trigger_insere_pessoa()
+    RETURNS trigger
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    endereco_id integer;
+    rec         record;
+    r           refcursor;
+BEGIN
+    OPEN r FOR SELECT * FROM endereco WHERE codigo_postal = NEW.endereco_codigo_postal
+                                        and pais = NEW.endereco_pais;
+    FETCH r INTO rec;
+
+    IF NOT FOUND THEN
+        INSERT INTO endereco (id, codigo_postal, cidade, estado, pais)
+        VALUES (DEFAULT,
+                NEW.endereco_codigo_postal,
+                NEW.endereco_cidade,
+                NEW.endereco_estado,
+                NEW.endereco_pais)
+        RETURNING id INTO endereco_id;
+    ELSE
+        endereco_id := rec.id;
+    END IF;
+    CLOSE r;
+
+    INSERT INTO pessoa (cpf,
+                        nome_registro,
+                        nome_social,
+                        nascimento,
+                        endereco_numero,
+                        endereco_rua,
+                        endereco_bairro,
+                        endereco_id)
+    VALUES (NEW.cpf,
+            NEW.nome,
+            NULL,
+            NEW.nascimento,
+            NEW.endereco_numero,
+            NEW.endereco_rua,
+            NEW.endereco_bairro,
+            endereco_id);
+
+    RETURN NEW;
+END;
+$$;
+
+CREATE OR REPLACE TRIGGER after_insert_view_admin_pessoa
+    INSTEAD OF INSERT
+    ON view_admin_pessoa
+    FOR EACH ROW
+EXECUTE FUNCTION trigger_insere_pessoa();
