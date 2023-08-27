c8915db314ccb6477d9390b2be9b6aa232ceddb4
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2023-08-27 11:17:53 -0300

    Seeding movido para o banco
    Isso permite usar o autocomplete de IDEs como o DataGrip.
    Ajuda a poupar esforço.

---
+++ database/sql/seed/endereco.sql
@@ -0,0 +1,11 @@
+INSERT INTO endereco
+    (codigo_postal, cidade, estado, pais)
+
+VALUES ('12345-678',
+        'São Paulo',
+        'SP',
+        'Brasil'),
+       ('12345-679',
+        'São Paulo',
+        'SP',
+        'Brasil');
