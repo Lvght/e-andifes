e9909d42ed68a33943e4c18b1f5a0ed76635b493
João Dini <copperbrjdmdini@gmail.com>
2023-08-30 17:17:23 -0300

    Update pr

--- database/sql/routines/pr_atualiza_turma_ofertada.sql
+++ database/sql/routines/pr_atualiza_turma_ofertada.sql
@@ -15,19 +15,16 @@ CREATE OR REPLACE PROCEDURE update_turma_ofertada_idioma(
 )
 AS $$
 DECLARE
-  -- declare an explicit cursor for the query
+  -- Cursor novo
   cur_turma_ofertada_idioma CURSOR FOR
     SELECT * FROM turma_ofertada_idioma WHERE id = p_id;
-  -- declare a record variable to hold the row
   rec_turma_ofertada_idioma turma_ofertada_idioma%ROWTYPE;
 BEGIN
-  -- open the cursor
   OPEN cur_turma_ofertada_idioma;
-  -- fetch the first row into the record variable
   FETCH cur_turma_ofertada_idioma INTO rec_turma_ofertada_idioma;
-  -- check if the row exists
+  -- Checa se a linha existe
   IF FOUND THEN
-    -- update the row with the new values
+    -- Update
     UPDATE turma_ofertada_idioma
     SET
         dia_semana = p_dia_semana,
@@ -42,12 +39,10 @@ BEGIN
         horario_termino = p_horario_termino,
         numero_inscritos = p_numero_inscritos,
         cpf_professor_isf = p_cpf_professor_isf
-    WHERE CURRENT OF cur_turma_ofertada_idioma; -- use the current row of the cursor
+    WHERE CURRENT OF cur_turma_ofertada_idioma;
   ELSE
-    -- raise an exception if the row does not exist
-    RAISE EXCEPTION 'Row with id % not found', p_id;
+    RAISE EXCEPTION 'Linha com id % não encontrada!', p_id;
   END IF;
-  -- close the cursor
   CLOSE cur_turma_ofertada_idioma;
 END;
 $$ LANGUAGE plpgsql;
