3b1d4beb51293009371192591672aa7bef5be973
MiguelChinellato <chinellato.miguel01@gmail.com>
2023-08-30 16:01:51 -0300

    Adicionando uso de cursor explicito em uma fn

--- database/sql/routines/fn_parceiros_inativos.sql
+++ database/sql/routines/fn_parceiros_inativos.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION parceiros_inativos()
+CREATE OR REPLACE FUNCTION fn_parceiros_inativos()
 RETURNS TABLE (
     cnpj VARCHAR(18),
     nome VARCHAR(100),
@@ -10,21 +10,43 @@ RETURNS TABLE (
     id INTEGER,
     status VARCHAR(20)
 ) AS $$
+DECLARE
+    cparceirosinativos CURSOR FOR
+        SELECT
+            p.cnpj,
+            p.nome,
+            p.numero,
+            p.rua,
+            p.bairro,
+            p.complemento,
+            p.cpf,
+            p.id,
+            p.status
+        FROM
+            parceiro p
+        WHERE
+            p.status = 'Inativo';
+    registro_parceiro_inativo RECORD;
 BEGIN
-    RETURN QUERY
-    SELECT
-        p.cnpj,
-        p.nome,
-        p.numero,
-        p.rua,
-        p.bairro,
-        p.complemento,
-        p.cpf,
-        p.id,
-	p.status
-    FROM
-        parceiro p
-    WHERE
-        p.status = 'Inativo';
+    OPEN cparceirosinativos;
+
+    LOOP
+        FETCH cparceirosinativos INTO registro_parceiro_inativo;
+        EXIT WHEN NOT FOUND;
+
+        cnpj := registro_parceiro_inativo.cnpj;
+        nome := registro_parceiro_inativo.nome;
+        numero := registro_parceiro_inativo.numero;
+        rua := registro_parceiro_inativo.rua;
+        bairro := registro_parceiro_inativo.bairro;
+        complemento := registro_parceiro_inativo.complemento;
+        cpf := registro_parceiro_inativo.cpf;
+        id := registro_parceiro_inativo.id;
+        status := registro_parceiro_inativo.status;
+
+        RETURN NEXT;
+    END LOOP;
+
+    CLOSE cparceirosinativos;
 END;
 $$ LANGUAGE plpgsql;
