8f2f3f1523d31940966e72c6e3b7edc0785d9abf
Lara S. Rodrigues <110498717+lsr94@users.noreply.github.com>
2023-08-31 15:33:20 -0300

    Alterando título do procedimento

--- database/sql/routines/pr_cadastra_turmas_oc.sql
+++ database/sql/routines/pr_cadastra_turmas_oc.sql
@@ -1,5 +1,5 @@
 -- Este procedimento cadastra as turmas da oferta coletiva
-CREATE OR REPLACE PROCEDURE pr_cadastra_turmas_oc2 (
+CREATE OR REPLACE PROCEDURE pr_cadastra_turmas_oc (
     p_dia_semana VARCHAR,
     p_horario_inicio TIME,
     p_idioma VARCHAR,
@@ -15,14 +15,11 @@ CREATE OR REPLACE PROCEDURE pr_cadastra_turmas_oc2 (
 )
 AS $$
 BEGIN
-	IF FOUND THEN -- Se a turma já estiver cadastrada
-		RAISE EXCEPTION 'Turma já cadastradada';
-	ELSE -- Caso a turma não estiver cadastrada, insere ela
-		INSERT INTO turma_ofertada_idioma (dia_semana, horario_inicio, idioma, dia_horario_aulas, titulo, data_inicio, 
-										   data_fim, tipo_oferta, horario_termino, numero_inscritos, id_curso, 
-										   cpf_professor_isf)
-		VALUES (p_dia_semana, p_horario_inicio, p_idioma, p_dia_horario_aulas, p_titulo, p_data_inicio, p_data_fim, 
-				p_tipo_oferta, p_horario_termino, p_numero_inscritos, p_id_curso, p_cpf_professor_isf);
-	END IF;
+    IF FOUND THEN -- Se a turma já estiver cadastrada
+	RAISE EXCEPTION 'Turma já cadastradada';
+    ELSE -- Caso a turma não estiver cadastrada, insere ela
+	INSERT INTO turma_ofertada_idioma (dia_semana, horario_inicio, idioma, dia_horario_aulas, titulo, data_inicio, data_fim, tipo_oferta, horario_termino, numero_inscritos, id_curso, cpf_professor_isf)
+	VALUES (p_dia_semana, p_horario_inicio, p_idioma, p_dia_horario_aulas, p_titulo, p_data_inicio, p_data_fim, p_tipo_oferta, p_horario_termino, p_numero_inscritos, p_id_curso, p_cpf_professor_isf);
+    END IF;
 END;
 $$ LANGUAGE plpgsql;
