bf5d0e8ca3e870daa0d1a90788fc0d94cbf680fc
MiguelChinellato <chinellato.miguel01@gmail.com>
2023-09-01 15:59:12 -0300

    Adicionando trigger instead of

---
+++ database/sql/triggers/instead_of_insert_view_cursos.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION fn_inserir_view_cursos()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Inserir os dados diretamente na tabela 'curso'
+    INSERT INTO curso (nome, codigo, idioma, carga_horaria, link_plano_de_ensino)
+    VALUES (NEW.nome, NEW.codigo, NEW.idioma, NEW.carga_horaria, NEW.link_plano_de_ensino);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER instead_of_insert_view_cursos
+INSTEAD OF INSERT ON view_cursos
+FOR EACH ROW
+EXECUTE FUNCTION fn_inserir_view_cursos();
