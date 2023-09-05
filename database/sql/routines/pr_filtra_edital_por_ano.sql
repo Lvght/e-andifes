871f3741962a970b1c7e18adefb6e3e0d6e78cc0
KY-U <uscaio@estudante.ufscar.br>
2023-09-05 19:39:37 -0300

    cursor explicito no filtro de edital por ano

--- database/sql/routines/pr_filtra_edital_por_ano.sql
+++ database/sql/routines/pr_filtra_edital_por_ano.sql
@@ -5,10 +5,18 @@ Descrição: filtra edital por ano
 
 CREATE PROCEDURE pr_filtra_edital_por_ano(ano_desejado integer)
 AS $$
+DECLARE
+    r   REFCURSOR;
+    rec RECORD;
 BEGIN
+    OPEN r FOR
     SELECT *
     FROM editais_abertos 
     WHERE ano = ano_desejado;
+    FETCH r INTO rec;
+    IF NOT FOUND THEN
+        RAISE EXCEPTION 'Não há editais abertos para o ano de %', ano_desejado;
+    END IF;
 END;
 $$
 LANGUAGE plpgsql;
