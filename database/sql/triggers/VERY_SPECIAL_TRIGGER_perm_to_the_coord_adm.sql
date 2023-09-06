cbfc4ddddf68a72644f333ac8888adafdb3431d2
PedroGSantos <pedrogsantos_10@hotmail.com>
2023-09-05 23:06:34 -0300

    fix: implementing Assertion on Trigger DML

--- database/sql/triggers/VERY_SPECIAL_TRIGGER_perm_to_the_coord_adm.sql
+++ database/sql/triggers/VERY_SPECIAL_TRIGGER_perm_to_the_coord_adm.sql
@@ -4,11 +4,37 @@ CREATE OR REPLACE FUNCTION tr_perm_delegate_to_the_coord_admin()
     LANGUAGE plpgsql
 AS
 $$
+DECLARE
+    role_exists BOOLEAN;
 BEGIN
+    
     IF TG_OP = 'DELETE' THEN
-        EXECUTE 'REVOKE coordenador_administrativo TO ' || quote_ident(OLD.cpf);
+        -- Verifica se o role existe
+        SELECT INTO role_exists
+        EXISTS (
+            SELECT 1
+            FROM pg_roles
+            WHERE rolname = OLD.cpf
+        );
+
+        -- Dispara uma exceção se o role não existir
+        ASSERT role_exists, 'Role não encontrado.';
+
+        EXECUTE 'REVOKE coordenador_administrativo FROM ' || quote_ident(OLD.cpf);
         RAISE NOTICE 'Cargo coordenador_administrativo revogado de %', OLD.cpf;
+
     ELSIF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' THEN
+        -- Verifica se o role existe
+        SELECT INTO role_exists
+        EXISTS (
+            SELECT 1
+            FROM pg_roles
+            WHERE rolname = NEW.cpf
+        );
+
+        -- Dispara uma exceção se o role não existir
+        ASSERT role_exists, 'Role não encontrado.';
+
         EXECUTE 'GRANT coordenador_administrativo TO ' || quote_ident(NEW.cpf);
         RAISE NOTICE 'Cargo coordenador_administrativo delegado a %', NEW.cpf;
         RETURN NEW;
