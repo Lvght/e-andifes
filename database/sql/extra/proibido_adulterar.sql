60c28d831fbaed257027641fee59eef3a05cccbb
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2023-09-06 12:24:46 -0300

    Várias melhorias

--- database/sql/extra/proibido_adulterar.sql
+++ database/sql/extra/proibido_adulterar.sql
@@ -4,3 +4,5 @@ CREATE RULE no_update_audit_log AS ON UPDATE TO registro_auditoria DO INSTEAD NO
 
 REVOKE ALL PRIVILEGES ON TABLE registro_auditoria FROM public;
 REVOKE ALL PRIVILEGES ON TABLE registro_auditoria FROM gestor_andifes;
+
+GRANT SELECT ON TABLE registro_auditoria TO public;
--- interface/src/public/editais/index.php
+++ interface/src/public/editais/index.php
@@ -2,11 +2,35 @@
 
 include $_ENV['HOME'] . '/inc/inject.php';
 
-if (isset($_GET['msg'])) {
-    $msg = $_GET['msg'];
-} else {
-    $msg = null;
-}
+try {
+    $query = pg_query(
+        getConnection(),
+        "SELECT id, nome, data_publicacao, ano, semestre, publicado_por, criado_em, criado_por FROM edital ORDER BY id DESC"
+    );
+    $editais = pg_fetch_all($query);
 
-$template = $twig->load('editais_visao_geral.html');
-echo $template->render(['cadastro_ok' => $msg == 'cadastro_ok']);
+    // Uso o array_merge para inserir os novos valores no começo do array
+    $editais = array_map(function ($x) {
+        // read link
+        $rlink = "/editais/detalhe.php?id={$x['id']}";
+        $x = array_merge(['Abrir' => $rlink], $x);
+        return $x;
+    }, $editais);
+
+    if (isset($_GET['msg'])) {
+        $msg = $_GET['msg'];
+    } else {
+        $msg = null;
+    }
+
+    if ($editais) {
+        $keys = array_keys($editais[0]);
+    } else {
+        $keys = [];
+    }
+
+    $template = $twig->load('editais_visao_geral.html');
+    echo $template->render(['cadastro_ok' => $msg == 'cadastro_ok', 'editais' => $editais, 'keys' => $keys]);
+} catch (Throwable $e) {
+    echo $e->getMessage();
+}
