68ce5164df9a87054baa21a4c2f358053bd92011
CarlosFontaneli <carlosfontaneli42@gmail.com>
2023-09-06 13:18:42 -0300

    interface gestor

--- database/sql/preconfig/create_roles.sql
+++ database/sql/preconfig/create_roles.sql
@@ -3,3 +3,5 @@ CREATE ROLE gestor_andifes INHERIT NOLOGIN;
 CREATE ROLE gestor_oferta_coletiva INHERIT NOLOGIN;
 
 GRANT especialista TO gestor_andifes;
+
+GRANT especialista to gestor_oferta_coletiva;
